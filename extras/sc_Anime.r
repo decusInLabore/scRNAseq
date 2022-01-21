0aac76539d7449610ea4344eed34bf4ef3e5e96c
Stefan Boeing <boeings@babs001.camp.thecrick.org>
2022-01-21 15:00:30 +0000

    Added frames

--- extras/sc_Anime.r
+++ extras/sc_Anime.r
@@ -20,7 +20,66 @@ db.pwd <- as.vector(dbTable[1,1])
 
 library(bioLOGIC)
 
-## Load first 
+###############################################################################
+## Create animation file                                                     ##
+
+
+createAnimationFrame <- function(
+    cellID = "cellID",
+    UMAP_1 = "UMAP_1",
+    UMAP_2 = "UMAP_2",
+    clusterName = "clusterName",
+    clusterColor = "clusterColor",
+    state = "stage name 1",
+    dfMeta = OsC@meta.data
+){
+    
+    dfTemp <- dfMeta
+    dfTemp <- dfMeta[,c(cellID,UMAP_1, UMAP_2, clusterName, clusterColor)]
+    dfTemp[["state"]] <- state
+    
+    dfTemp <- dfTemp[!is.na(dfTemp[,UMAP_1]), ]
+    dfTemp <- dfTemp[!is.na(dfTemp[,UMAP_2]), ]
+    dfTemp <- dfTemp[dfTemp[,UMAP_1] != 0 & dfTemp[,UMAP_2] != 0, ]
+    
+    if (is.null(clusterColor)){
+        colOpts <- sort(unique(dfTemp[,clusterName]))
+        cols <- scales::hue_pal()(length(colOpts))
+        dfCol <- data.frame(colOpts, cols)
+        names(dfCol) <- c(clusterName, "clusterColor")
+        
+        dfTemp <- dplyr::full_join(
+            dfTemp,
+            dfCol,
+            by = clusterName
+        )
+        
+    }
+    
+    names(dfTemp) <- gsub(cellID, "cellID", names(dfTemp))
+    names(dfTemp) <- gsub(UMAP_1, "UMAP_1", names(dfTemp))
+    names(dfTemp) <- gsub(UMAP_2, "UMAP_2", names(dfTemp))
+    names(dfTemp) <- gsub(clusterName, "clusterName", names(dfTemp))
+    
+    if (!is.null(clusterColor)){
+        names(dfTemp) <- gsub(clusterColor, "clusterColor", names(dfTemp))
+    }
+    
+    dfAdd <- dfTemp[, c("cellID","UMAP_1", "UMAP_2", "clusterName", "clusterColor", "state")]
+    return(dfAdd)
+}
+
+
+
+## Done                                                                      ##
+###############################################################################
+
+
+###############################################################################
+## First frame                                                               ##
+
+stateName <- "Experiment A4A6A7A8 Before Subclustering"
+orderVec <- c(stateName)
 
 dfMeta <- import.db.table.from.db(
     dbtable = "A4A6A7A8_PCA",
@@ -30,51 +89,165 @@ dfMeta <- import.db.table.from.db(
     host     = "10.27.241.82"
 )
 
-dfMeta[["seuratSampleID"]] <- sapply(dfMeta$cellID, function(x) unlist(strsplit(x, "_"))[2])
-dfMeta[["mergeID"]] <- sapply(dfMeta$cellID, function(x) unlist(strsplit(x, "_"))[1])
-
-dfMeta[["merge"]] <- paste0(dfMeta$sampleID, "_", dfMeta$mergeID)
-
-#dfMeta <- dfMeta[,c("cellID","UMAP_1", "UMAP_2", "sampleName", "sampleColor", "clusterName", "clusterColor")]
-
-dfMeta <- dfMeta[,c("cellID","UMAP_1", "UMAP_2", "clusterName", "clusterColor")]
-dfMeta[["state"]] <- "Before Subclustering"
-
-## Load second
-dfMeta2 <- import.db.table.from.db(
-    dbtable = "A4A6A7A8sub_PCA",
-    dbname = "pbl_data",
-    user     = "boeingS",
-    password = db.pwd,
-    host     = "10.27.241.82"
+dfAdd <- createAnimationFrame(
+    cellID = "cellID",
+    UMAP_1 = "UMAP_1",
+    UMAP_2 = "UMAP_2",
+    clusterName = "clusterName",
+    clusterColor = NULL,
+    state = stateName,
+    dfMeta = dfMeta
 )
 
-dfMeta2 <- dfMeta2[,c("cellID","UMAP_1", "UMAP_2", "sampleName", "sampleColor", "clusterName", "clusterColor")]
-dfMeta2 <- dfMeta2[,c("cellID","UMAP_1", "UMAP_2", "clusterName", "clusterColor")]
-dfMeta2[["state"]] <- "New UMAP After Subclustering A4A6A7A8sub"
+## Get cluster colors ##
+FNcol <- paste0(hpc.mount, "projects/bonfantip/roberta.ragazzini/471_scRNAseq_newSamples_Epi6_Epi10_SC20193_SC21201/workdir/scRNAseq/design/clusterAnnotation.txt")
+
+dfCol <- read.delim(
+    FNcol, 
+    header = T,
+    sep = "\t",
+    stringsAsFactors = F
+)
+
+dfCol <- unique(dfCol[,c("clusterName", "clusterColor")])
+
+dfAdd$clusterColor <- NULL
+
+dfAdd <- dplyr::full_join(
+    dfAdd,
+    dfCol,
+    by = clusterName
+)
 
 
 
+dfReel <- dfAdd
 
-
-## Add transition state
-dfMetaT1 <- dfMeta
-dfMetaT1[["state"]] <- "Cells Marked for Removal in A4A6A7A8"
-dfMetaT1$clusterName <- gsub("C4", "Remove",dfMetaT1$clusterName)
-dfMetaT1$clusterName <- gsub("C8", "Remove",dfMetaT1$clusterName)
-dfMetaT1$clusterName <- gsub("C11", "Remove",dfMetaT1$clusterName)
-dfMetaT1$clusterName <- gsub("C13", "Remove",dfMetaT1$clusterName)
-dfMetaT1[dfMetaT1$clusterName == "Remove", "clusterColor"] <- "#000000"
-
-
-
-dfMetaT2 <- dfMetaT1
-dfMetaT2[["state"]] <- "UMAP A4A6A7A8 After Cell Removal"
-dfMetaT2 <- dfMetaT2[-grep("Remove", dfMetaT2$clusterName), ]
+## Done                                                                      ##
+###############################################################################
 
 ###############################################################################
-## Add cell cycle states                                                     ##
-dfMetaCS <- import.db.table.from.db(
+## Second Frame                                                              ##
+## Change color for clusters to be removed to black
+## Clusters marked for removal: C4, C8, C11, C13
+
+stateName <- "Contaminating cells to be removed are marked black"
+orderVec <- c(orderVec, stateName)
+
+dfMeta <- import.db.table.from.db(
+    dbtable = "A4A6A7A8_PCA",
+    dbname = "pbl_data",
+    user     = "boeingS",
+    password = db.pwd,
+    host     = "10.27.241.82"
+)
+
+dfAdd <- createAnimationFrame(
+    cellID = "cellID",
+    UMAP_1 = "UMAP_1",
+    UMAP_2 = "UMAP_2",
+    clusterName = "clusterName",
+    clusterColor = NULL,
+    state = stateName,
+    dfMeta = dfMeta
+)
+
+## Get cluster colors ##
+FNcol <- paste0(hpc.mount, "projects/bonfantip/roberta.ragazzini/471_scRNAseq_newSamples_Epi6_Epi10_SC20193_SC21201/workdir/scRNAseq/design/clusterAnnotation.txt")
+
+dfCol <- read.delim(
+    FNcol, 
+    header = T,
+    sep = "\t",
+    stringsAsFactors = F
+)
+
+dfCol <- unique(dfCol[,c("clusterName", "clusterColor")])
+
+dfCol[dfCol$clusterName %in% c("C4", "C8", "C11", "C13"),"clusterColor"] <- "#000000"
+
+dfAdd$clusterColor <- NULL
+
+dfAdd <- dplyr::full_join(
+    dfAdd,
+    dfCol,
+    by = clusterName
+)
+
+dfReel <- rbind(
+    dfReel, 
+    dfAdd
+)
+
+## Done                                                                      ##
+###############################################################################
+
+###############################################################################
+## Third Frame                                                               ##
+## Change color for clusters to be removed to black
+## Clusters marked for removal: C4, C8, C11, C13
+stateName <- "Contaminating cells removed from A4A6A7A8"
+orderVec <- c(orderVec, stateName)
+
+dfMeta <- import.db.table.from.db(
+    dbtable = "A4A6A7A8_PCA",
+    dbname = "pbl_data",
+    user     = "boeingS",
+    password = db.pwd,
+    host     = "10.27.241.82"
+)
+
+dfAdd <- createAnimationFrame(
+    cellID = "cellID",
+    UMAP_1 = "UMAP_1",
+    UMAP_2 = "UMAP_2",
+    clusterName = "clusterName",
+    clusterColor = NULL,
+    state = stateName,
+    dfMeta = dfMeta
+)
+
+## Get cluster colors ##
+FNcol <- paste0(hpc.mount, "projects/bonfantip/roberta.ragazzini/471_scRNAseq_newSamples_Epi6_Epi10_SC20193_SC21201/workdir/scRNAseq/design/clusterAnnotation.txt")
+
+dfCol <- read.delim(
+    FNcol, 
+    header = T,
+    sep = "\t",
+    stringsAsFactors = F
+)
+
+dfCol <- unique(dfCol[,c("clusterName", "clusterColor")])
+
+dfCol[dfCol$clusterName %in% c("C4", "C8", "C11", "C13"),"clusterColor"] <- "#000000"
+
+dfAdd$clusterColor <- NULL
+
+dfAdd <- dplyr::full_join(
+    dfAdd,
+    dfCol,
+    by = clusterName
+)
+
+dfAdd <- dfAdd[!(dfAdd$clusterName %in% c("C4", "C8", "C11", "C13")), ]
+
+dfReel <- rbind(
+    dfReel, 
+    dfAdd
+)
+
+## Done                                                                      ##
+###############################################################################
+
+###############################################################################
+## Fortht Frame                                                              ##
+## Change color for clusters to be removed to black
+## Clusters marked for removal: C4, C8, C11, C13
+
+stateName <- "New UMAP for Cell Selection A4A6A7A8sub"
+orderVec <- c(orderVec, stateName)
+
+dfMeta <- import.db.table.from.db(
     dbtable = "A4A6A7A8sub_PCA",
     dbname = "pbl_data",
     user     = "boeingS",
@@ -82,28 +255,119 @@ dfMetaCS <- import.db.table.from.db(
     host     = "10.27.241.82"
 )
 
-dfWO <- dfMetaCS[dfMetaCS$UMAP_1_Without_Regression != 0 & dfMetaCS$UMAP_2_Without_Regression != 0 ,c("cellID", "UMAP_1_Without_Regression", "UMAP_2_Without_Regression", "ClusterNames_CellCycle_Regression",  "clusterColor")]
+dfAdd <- createAnimationFrame(
+    cellID = "cellID",
+    UMAP_1 = "UMAP_1",
+    UMAP_2 = "UMAP_2",
+    clusterName = "clusterName",
+    clusterColor = NULL,
+    state = stateName,
+    dfMeta = dfMeta
+)
+
+## Get cluster colors ##
+FNcol <- paste0(hpc.mount, "projects/bonfantip/roberta.ragazzini/471_subset_scRNAseq_newSamples_Epi6_Epi10_SC20193_SC21201/workdir/scRNAseq/design/clusterAnnotation.txt")
+
+dfCol <- read.delim(
+    FNcol, 
+    header = T,
+    sep = "\t",
+    stringsAsFactors = F
+)
+
+dfCol <- dfCol[,c("clusterName", "clusterColor")]
+
+dfAdd$clusterColor <- NULL
+
+dfAdd <- dplyr::full_join(
+    dfAdd,
+    dfCol,
+    by = clusterName
+)
+
+dfReel <- rbind(
+    dfReel, 
+    dfAdd
+)
+
+## Done                                                                      ##
+###############################################################################
+
+###############################################################################
+## Fifth Frame                                                               ##
+## Change color for clusters to be removed to black
+## Clusters marked for removal: C4, C8, C11, C13
+
+stateName <- "Cell Cycle Phases A4A6A7A8sub - No regression"
+orderVec <- c(orderVec, stateName)
+
+dfMeta <- import.db.table.from.db(
+    dbtable = "A4A6A7A8sub_PCA",
+    dbname = "pbl_data",
+    user     = "boeingS",
+    password = db.pwd,
+    host     = "10.27.241.82"
+)
+
+dfAdd <- createAnimationFrame(
+    cellID = "cellID",
+    UMAP_1 = "UMAP_1_Without_Regression",
+    UMAP_2 = "UMAP_2_Without_Regression",
+    clusterName = "Phase",
+    clusterColor = NULL,
+    state = stateName,
+    dfMeta = dfMeta
+)
+
+
+
+dfReel <- rbind(
+    dfReel, 
+    dfAdd
+)
+
+## Done                                                                      ##
+###############################################################################
+
+###############################################################################
+## Sixtht Frame                                                              ##
+## Change color for clusters to be removed to black
+## Clusters marked for removal: C4, C8, C11, C13
+
+stateName <- "Cell Cycle Phases A4A6A7A8sub - Full Cell Cycle Regression"
+orderVec <- c(orderVec, stateName)
+
+dfMeta <- import.db.table.from.db(
+    dbtable = "A4A6A7A8sub_PCA",
+    dbname = "pbl_data",
+    user     = "boeingS",
+    password = db.pwd,
+    host     = "10.27.241.82"
+)
+
+dfAdd <- createAnimationFrame(
+    cellID = "cellID",
+    UMAP_1 = "UMAP_1_CellCycle_Regression",
+    UMAP_2 = "UMAP_2_CellCycle_Regression",
+    clusterName = "Phase",
+    clusterColor = NULL,
+    state = stateName,
+    dfMeta = dfMeta
+)
+
+
+
+dfReel <- rbind(
+    dfReel, 
+    dfAdd
+)
 
 ## Done                                                                      ##
 ###############################################################################
 
 
-dfReel <- rbind(
-    dfMeta, 
-    dfMetaT1,
-    dfMetaT2,
-    dfMeta2
-)
+dfReel$state <- factor(dfReel$state, levels = orderVec)
 
-dfReel$state <- factor(dfReel$state, levels = c(
-    "Before Subclustering", 
-    "Cells Marked for Removal in A4A6A7A8", 
-    "UMAP A4A6A7A8 After Cell Removal", 
-    "New UMAP After Subclustering A4A6A7A8sub"
-    )
-)
-
-library(gganimate)
 #> Loading required package: ggplot2
 
 # We'll start with a static plot
@@ -113,9 +377,20 @@ minX <- 1.1 * min(dfReel$UMAP_1)
 maxY <- 1.1 * max(dfReel$UMAP_2)
 minY <- 1.1 * min(dfReel$UMAP_2)
 
-p <- ggplot(dfReel, aes(x = UMAP_1, y = UMAP_2, color = clusterColor)) +
-    geom_point() + ggplot2::theme_bw(
-    )  +  ggplot2::theme(
+## Set dotsize on first frame
+avgSize <- round(nrow(dfReel) / length(orderVec))
+dotsize  = 1
+if (avgSize  > 10000){
+    dotsize  = 0.75
+} else if (avgSize  > 20000){
+    dotsize = 0.5
+} else if (avgSize  > 50000){
+    dotsize = 0.25
+}
+
+p <- ggplot(dfReel, aes(x = UMAP_1, y = UMAP_2, color = clusterColor)
+    ) + geom_point(size = as.numeric(dotsize)) + ggplot2::theme_bw(
+    ) +  ggplot2::theme(
         axis.text.y   = ggplot2::element_text(size=8),
         axis.text.x   = ggplot2::element_text(size=8),
         axis.title.y  = ggplot2::element_text(size=8),
@@ -124,33 +399,35 @@ p <- ggplot(dfReel, aes(x = UMAP_1, y = UMAP_2, color = clusterColor)) +
         panel.border = ggplot2::element_rect(colour = "black", fill=NA, size=1),
         plot.title = ggplot2::element_text(hjust = 0.5, size = 12),
         legend.title = ggplot2::element_blank()
-    ) + scale_colour_identity() + labs(title = 'Stage: {closest_state}') + ggplot2::coord_fixed(ratio=1
-    )  + xlim(c(minX, maxX)) + ylim(c(minY, maxY))
+    ) + scale_colour_identity() + labs(title = 'Stage: {closest_state}'
+    ) + ggplot2::coord_fixed(ratio=1
+    ) + xlim(c(minX, maxX)) + ylim(c(minY, maxY))
 
 ## Check
 p2 <- p + facet_wrap( ~ state,  ncol = 2)
 
 ###############################################################################
 ## Add cluster labels in plot                                                ##
-library(dplyr)
-dfTextPos <- dfReel %>% group_by(clusterName, clusterColor) %>% select(UMAP_1, UMAP_2) %>% summarize_all(mean)
-p <- p + ggrepel::geom_label_repel(data = dfTextPos,aes(label = clusterName))
+# library(dplyr)
+# dfTextPos <- dfReel %>% group_by(clusterName, clusterColor) %>% select(UMAP_1, UMAP_2) %>% summarize_all(mean)
+# p <- p + ggrepel::geom_label_repel(data = dfTextPos,aes(label = clusterName))
 ##
 ###############################################################################
 
 
-library(gganimate)
-anim <- p +
-    gganimate::transition_states(state,
-                      transition_length = 1,
-                      state_length = 5,
-                      wrap = FALSE)
+anim <- p + gganimate::transition_states(
+    state,
+    transition_length = 1,
+    state_length = 5,
+    wrap = FALSE
+)
 
 animate(anim, fps=5)
 
 
 ###############################################################################
 ## Save                                                                      ##
+dir.create("test_anim")
 animate(anim, nframes = 24, device = "png",
         renderer = file_renderer("test_anim", prefix = "A4A5_UMAP", overwrite = TRUE))
 
