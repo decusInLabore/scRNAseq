f7cd35f16a4562167637cf3bea42eaac9b243fe0
Stefan Boeing <boeings@babs001.camp.thecrick.org>
2022-03-15 11:54:19 +0000

    Set B21 PCA loadings to false by default pbl478

--- runAll.sh
+++ runAll.sh
@@ -12,14 +12,14 @@ sleep 300
 done
 }
 
-project_id=471sub
+project_id=478
 
 project=rA_$project_id
-sbatch --time=24:00:00 --wrap "module purge;source /camp/stp/babs/working/software/modulepath_new_software_tree_2018-08-13;module load pandoc/2.2.3.2-foss-2016b;ml R/4.0.3-foss-2020a;Rscript runRmd.r sc_PartA_QC.Rmd" --job-name=$project -p hmem --mem=300G -o ../rA.$project.slurm >> ../commands.txt
+sbatch --time=48:00:00 --wrap "module purge;source /camp/stp/babs/working/software/modulepath_new_software_tree_2018-08-13;module load pandoc/2.2.3.2-foss-2016b;ml R/4.0.3-foss-2020a;Rscript runRmd.r sc_PartA_QC.Rmd" --job-name=$project -p hmem --mem=300G -o ../rA.$project.slurm >> ../commands.txt
 
 wait_on_lsf
 project=rB_$project_id
-sbatch --time=24:00:00 --wrap "module purge;source /camp/stp/babs/working/software/modulepath_new_software_tree_2018-08-13;module load pandoc/2.2.3.2-foss-2016b;ml R/4.0.3-foss-2020a;Rscript runRmd.r sc_PartB_Analysis.Rmd" --job-name=$project -p hmem --mem=300G -o ../rB.$project.slurm >> ../commands.txt
+sbatch --time=36:00:00 --wrap "module purge;source /camp/stp/babs/working/software/modulepath_new_software_tree_2018-08-13;module load pandoc/2.2.3.2-foss-2016b;ml R/4.0.3-foss-2020a;Rscript runRmd.r sc_PartB_Analysis.Rmd" --job-name=$project -p hmem --mem=300G -o ../rB.$project.slurm >> ../commands.txt
 
 wait_on_lsf
 project=rCF_$project_id
