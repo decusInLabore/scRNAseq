0d1773233f63e59eb55d790cc4bb43172de9e522
Stefan Boeing <boeings@babs000.camp.thecrick.org>
2022-09-29 13:46:45 +0100

    Rcpp package ref updated

--- runAll.sh
+++ runAll.sh
@@ -12,10 +12,10 @@ sleep 300
 done
 }
 
-project_id=470
+project_id=511
 
 project=rA_$project_id
-sbatch --time=48:00:00 --wrap "module purge;source /camp/stp/babs/working/software/modulepath_new_software_tree_2018-08-13;module load pandoc/2.2.3.2-foss-2016b;ml R/4.0.3-foss-2020a;Rscript runRmd.r sc_PartA_QC.Rmd" --job-name=$project -p hmem --mem=300G -o ../rA.$project.slurm >> ../commands.txt
+sbatch --time=48:00:00 --wrap "module purge;source /camp/stp/babs/working/software/modulepath_new_software_tree_2018-08-13;module load pandoc/2.2.3.2-foss-2016b;ml R/4.0.3-foss-2020a;Rscript runRmd.r sc_PartA_QC.Rmd" --job-name=$project --mem=200G -o ../rA.$project.slurm >> ../commands.txt
 
 wait_on_lsf
 project=rB_$project_id
