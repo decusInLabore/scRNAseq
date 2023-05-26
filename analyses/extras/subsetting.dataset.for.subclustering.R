051a0d728d6b7f7a520a0e2efd9f627b257dd520
Stefan Boeing <boeings@babs000.camp.thecrick.org>
2023-05-26 12:52:59 +0100

    updated rlang

--- analyses/extras/subsetting.dataset.for.subclustering.R
+++ analyses/extras/subsetting.dataset.for.subclustering.R
@@ -28,43 +28,40 @@ if (!file.exists("renv.lock")){
 library(Seurat)
 ## load Seurat object with basedata
 
-baseDir <- "/camp/stp/babs/working/boeings/Projects/sahaie/rebecca.lee/506_mouse_liver_single_cell_SC20154_plus_SC22147/"
-FN <- paste0(baseDir, "workdir/SC22147.Seurat.Robj")
+baseDir <- "/camp/stp/babs/working/boeings/Projects/schaefera/tobias.ackels/319A_scRNAseq_mm_olfactory_bulb_proj_neuron_analysis_SC19135/"
+FN <- paste0(baseDir, "workdir/asl319A.Seurat.Robj")
 
 load(FN)
 
 ## Data selection for subsetting:
-#sampleID: primaryTumor
-
-# To subcluster melanoma cells (current clusters Melanoma_1 and renamed Melanoma_2) 
-# in samples 2,4, and 5
-
-# BackgroundLiverM3	  1
-# LiverMetM3	        2
-# LiverNormalM1	      3
-# SubcutM3	          4
-# SubcutNoLiverMetM2	5
+# subcluser selection GC1, GC2, GC3, GC4, GC5, GC6, GC7
 
 # and to compare proportions of different sub clusters. Deliverables: Subclustering of Melanoma_1 and Melanoma_2 subclusters in samples 2, 3, and 5 in analysis SC22147, summarized in a standard single-cell analysis report. 
 
 ## Review table
 unique(OsC@meta.data[,c("clusterName", "seurat_clusters")])
 
-# Melanoma               3
-#  Melanoma_2           13
+#AAACCCAAGTAAATGC_2         GC1               0
+#AAACCCAAGAAGGATG_1         GC2               1
+#AAACCCACAGCCTTCT_1         GC3               3
+#AAACCCATCGGCTGTG_2         GC4              10
+#AAACCCAGTGGTCTAT_1         GC5              11
+#AAACCCAAGGATACAT_2         GC6              12
+#AAACGCTCATGCCATA_2         GC7              17
+
 
 # Select only cluster 1
-selClusters <- c(3, 13)
+selClusters <- c(0, 1, 3, 10, 11, 12,17)
 OsC_sel1 <- subset(x = OsC, subset = seurat_clusters %in% selClusters)
 
 # select samples 2, 4 and 5
-selSamples <- c(
-  "LiverMetM3R1", "LiverMetM3R2",
-  "SubcutM3R1", "SubcutM3R2",
-  "SubcutNoLiverMetM2R1", "SubcutNoLiverMetM2R2"
-)
+#selSamples <- c(
+#  "LiverMetM3R1", "LiverMetM3R2",
+#  "SubcutM3R1", "SubcutM3R2",
+#  "SubcutNoLiverMetM2R1", "SubcutNoLiverMetM2R2"
+#)
 
-OsC_sel1 <- subset(x = OsC_sel1, subset = sampleName %in% selSamples)
+#OsC_sel1 <- subset(x = OsC_sel1, subset = sampleName %in% selSamples)
 
 sampleIDs <- unique(OsC_sel1$sampleID)
 
