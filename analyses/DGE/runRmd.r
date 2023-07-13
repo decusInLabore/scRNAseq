b56164d018c529c9506a441bc4fc2cb14d0f10da
Stefan Boeing <boeings@babs000.camp.thecrick.org>
2023-07-13 15:05:19 +0100

    Updates

--- analyses/DGE/runRmd.r
+++ analyses/DGE/runRmd.r
@@ -1,13 +1,3 @@
 args = commandArgs(trailingOnly = T)
 
 rmarkdown::render(args[1], output_dir ="../../../../html_local")
-
-# if (!file.exists("index.Rmd")){
-#   file.create("index.Rmd")
-# }
-# 
-# if (!file.exists("_site.yml")){
-#   file.create("_site.yml")
-# }
-# 
-# rmarkdown::render_site(args[1])
